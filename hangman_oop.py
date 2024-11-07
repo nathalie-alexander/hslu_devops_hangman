89a4e26332dde2f8f7cb25b64f838d742602d7fb
nathalie-alexander <nathalie.alexander@gmx.at>
2024-11-07 22:41:49 +0100

    added players and scores to the game

--- hangman_oop.py
+++ hangman_oop.py
@@ -1,4 +1,14 @@
 import os
+import random
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def add_point(self):
+        self.score += 1
+
 
 class Hangman:
     HANGMAN_PICS = ['''
@@ -60,11 +70,34 @@ class Hangman:
 =========''']
 
     def __init__(self):
+        self.players = self.initialize_players()
+        self.current_player_index = 0
+        self.word_list = ["python", "hangman", "developer", "challenge", "programming", "algorithm", "data", "structure"]
         self.reset_game()
+
+    def initialize_players(self):
+        players = []
+        num_players = int(input("Enter the number of players: "))
+        for i in range(num_players):
+            name = input(f"Enter the name of player {i + 1}: ")
+            players.append(Player(name))
+        self.use_random_word = num_players < 2
+        return players
+
+    # Switch to the next player
+    def next_player(self):
+        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+
+    # Get the current player
+    def current_player(self):
+        return self.players[self.current_player_index]
 
     def reset_game(self):
         # Reset game state
-        self.word_to_guess = input("Enter a word for others to guess: ").strip().upper()
+        if self.use_random_word:
+            self.word_to_guess = random.choice(self.word_list).upper()
+        else:
+            self.word_to_guess = input(f"Enter a word for {self.current_player().name} to guess: ").strip().upper()
         self.clear_screen()
         self.guessed_letters = set()
         self.wrong_guesses = 0
@@ -81,6 +114,12 @@ class Hangman:
     def display_word(self):
         display = [letter if letter in self.correct_guesses else '_' for letter in self.word_to_guess]
         return ' '.join(display)
+    
+    def display_scores(self):
+        print("Scores:")
+        for player in self.players:
+            print(f"{player.name}: {player.score} points")
+        print("\n")
 
     def display_hangman(self):
         print(self.HANGMAN_PICS[self.wrong_guesses])
@@ -132,6 +171,7 @@ class Hangman:
             self.display_hangman()
 
             if self.game_status() == "won":
+                self.current_player().add_point()
                 print("""
                                     .''.       
         .''.      .        *''*    :_\/_:     . 
@@ -144,7 +184,9 @@ class Hangman:
         *
             *
                     """)
-                print(f"\nCongratulations! You've won! The word was '{self.word_to_guess}'.")
+                print(f"""Congratulations {self.current_player().name}! You've won! The word was '{self.word_to_guess}'.
+                      Score: {self.current_player().score} points
+                        """)
             else:
                 print("""
     ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
@@ -175,9 +217,12 @@ class Hangman:
                     """)
                 print(f"\nGame over! The word was '{self.word_to_guess}'.")
 
+            self.display_scores()
+            
             if not self.play_again():
                 print("Thanks for playing Hangman! Goodbye!")
                 break
+            self.next_player()
             self.reset_game()
 
 # Start the game
